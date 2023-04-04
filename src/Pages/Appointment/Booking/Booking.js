53c05fcfdbcbd0c1d6b0211347d52fe35e9e0579
Aymanoli <aymanoli777@gmail.com>
2023-04-04 15:20:55 +0600

    bookingmodal

--- src/Pages/Appointment/Booking/Booking.js
+++ src/Pages/Appointment/Booking/Booking.js
@@ -3,30 +3,38 @@ import React from 'react';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import BookingModal from '../BookingModal/BookingModal';
 
 
 
 const Booking = ({booking}) => {
     const {name, time, space} = booking;
+    const [openBooking, setBookingOpen] = React.useState(false);
+    const handleBookingOpen = () => setBookingOpen(true);
+    const handleBookingClose = () => setBookingOpen(false);
     return (
         
-            <Grid item xs={12} sm={6} md={4}>
-            <Paper elevation={3} sx={{py:5}}>
-             <Typography variant="h5" gutterBottom sx={{fontWeight: 600}}>
-             {name}
-           </Typography>
-             <Typography variant="h6" gutterBottom sx={{fontWeight: 400}}>
-             {time}
-           </Typography>
-           <Typography variant="caption" display="block" gutterBottom>
-        {space} SPACE AVAILABLE
-      </Typography>
-      <Button style={{background: 'linear-gradient(90deg,#19D3AE,#0FCFEC)'}} variant="contained">Book Appointment</Button>
-      
-
-            </Paper>
-           
-         </Grid>
+     <>
+              <Grid item xs={12} sm={6} md={4}>
+              <Paper elevation={3} sx={{py:5}}>
+              <Typography variant="h5" gutterBottom sx={{fontWeight: 600}}>
+              {name}
+            </Typography>
+              <Typography variant="h6" gutterBottom sx={{fontWeight: 400}}>
+              {time}
+            </Typography>
+            <Typography variant="caption" display="block" gutterBottom>
+          {space} SPACE AVAILABLE
+        </Typography>
+        <Button style={{background: 'linear-gradient(90deg,#19D3AE,#0FCFEC)'}} onClick={handleBookingOpen} variant="contained">Book Appointment</Button>
+        </Paper>
+        </Grid>
+        <BookingModal
+        booking={booking}
+        openBooking={openBooking}
+        handleBookingClose={handleBookingClose}
+        ></BookingModal>
+     </>
         
     );
 };
