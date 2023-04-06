2376702b67b918c676966a471615e4da3524e87c
Aymanoli <aymanoli777@gmail.com>
2023-04-06 15:19:05 +0600

    login

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import {
   Route,
   Routes
 } from "react-router-dom";
-import Home from './Pages/Home/Home/Home'; 
+import Home from './Pages/Home/Home/Home';
 import Appointment from './Pages/Appointment/Appointment/Appointment';
+import Login from './Pages/Login/Login/Login';
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/home' element={<Home/>}></Route>
-          <Route path='/appointment' element={<Appointment/>}></Route>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/appointment' element={<Appointment />}></Route>
         </Routes>
       </Router>
     </div>
