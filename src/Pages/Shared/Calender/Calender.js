87662b518ec7ec2fe0b72d81bab011b772eff571
Aymanoli <aymanoli777@gmail.com>
2023-08-09 23:14:17 +0600

    nav update

--- src/Pages/Shared/Calender/Calender.js
+++ src/Pages/Shared/Calender/Calender.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-// import dayjs from 'dayjs';
 
 const Calender = ({ date, setDate }) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DateCalendar
-        // defaultValue={dayjs('lll')}
         date={date} onChange={(newDate) => setDate(newDate)}
       />
     </LocalizationProvider>
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -10,43 +10,41 @@ import { Link, NavLink } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 
 const Navigation = () => {
-  const {user, logOut} = useAuth();
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Doctors Portal
-            </Typography>
-            <Link style={{textDecoration: 'none', color: 'white'}} to="/appointment"><Button color="inherit">Appointment</Button></Link>
-            {
-              user?.email? 
-              // v 72.6
-                <Box>
-                  <NavLink style={{textDecoration: 'none', color: 'white'}} to="/dashboard">
+  const { user, logOut } = useAuth();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Doctors Portal
+          </Typography>
+          <Link style={{ textDecoration: 'none', color: 'white' }} to="/appointment"><Button color="inherit">Appointment</Button></Link>
+          {
+            user?.email ?
+              <Box>
+                <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/dashboard">
                   <Button color="inherit">Dashboard</Button>
-                  </NavLink>
-                  <Button onClick={logOut} color="inherit">LogOut</Button>
-                </Box>
-              // v 72.6
-                :
-              <NavLink style={{textDecoration: 'none', color: 'white'}} to="/login">
+                </NavLink>
+                <Button onClick={logOut} color="inherit">LogOut</Button>
+              </Box>
+              :
+              <NavLink style={{ textDecoration: 'none', color: 'white' }} to="/login">
                 <Button color="inherit">Login</Button>
               </NavLink>
-            }
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
+          }
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Navigation;
